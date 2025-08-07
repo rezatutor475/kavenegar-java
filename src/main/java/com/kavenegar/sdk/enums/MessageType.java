28c23d80f4446c48d538244fd3a90ef78ab17d17
Reza Torabi <136625739+rezatutor475@users.noreply.github.com>
2025-08-07 18:07:43 +0330

    Update MessageType.java
    getLabel() – to return a user-friendly label.
    A getLabel() method for human-readable labels.
    isValidType(int type) – to validate if a given integer is a valid message type.
    An isValidType(int) method to validate input integers.

--- src/main/java/com/kavenegar/sdk/enums/MessageType.java
+++ src/main/java/com/kavenegar/sdk/enums/MessageType.java
@@ -5,8 +5,9 @@
 package com.kavenegar.sdk.enums;
 
 /**
+ * Represents the different types of messages that can be sent.
  *
- * @author mohsen
+ * The code was originally written by Mohsen and later developed further by Reza.
  */
 public enum MessageType {
 
@@ -14,16 +15,28 @@ public enum MessageType {
     MobileMemory(1),
     SimMemory(2),
     AppMemory(3);
+
     private final int value;
 
     private MessageType(int type) {
         this.value = type;
     }
 
+    /**
+     * Gets the integer value associated with the message type.
+     *
+     * @return the integer value of the message type
+     */
     public int getValue() {
         return value;
     }
 
+    /**
+     * Returns the MessageType corresponding to the given integer value.
+     *
+     * @param type the integer representation of the MessageType
+     * @return the MessageType if found, otherwise null
+     */
     public static MessageType valueOf(int type) {
         for (MessageType code : MessageType.values()) {
             if (type == code.getValue()) {
@@ -31,5 +44,35 @@ public enum MessageType {
             }
         }
         return null;
+    }
+
+    /**
+     * Gets a human-readable label for the message type.
+     *
+     * @return a string representation of the message type
+     */
+    public String getLabel() {
+        switch (this) {
+            case Flash:
+                return "Flash Message";
+            case MobileMemory:
+                return "Mobile Memory Message";
+            case SimMemory:
+                return "SIM Memory Message";
+            case AppMemory:
+                return "App Memory Message";
+            default:
+                return "Unknown Message Type";
+        }
+    }
+
+    /**
+     * Validates if the given integer is a valid MessageType.
+     *
+     * @param type the integer to validate
+     * @return true if the value corresponds to a MessageType, otherwise false
+     */
+    public static boolean isValidType(int type) {
+        return valueOf(type) != null;
     }
 }
