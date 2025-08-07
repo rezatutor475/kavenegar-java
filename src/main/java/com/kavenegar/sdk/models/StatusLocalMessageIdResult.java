ee06fb496d515e160efc63751b095f01fda42ff7
Reza Torabi <136625739+rezatutor475@users.noreply.github.com>
2025-08-07 10:45:06 +0330

    Update StatusLocalMessageIdResult.java
    hasSameLocalId(...): Compares two results' local IDs.
    
    isFailedStatus(): Quickly checks if the status indicates failure.

--- src/main/java/com/kavenegar/sdk/models/StatusLocalMessageIdResult.java
+++ src/main/java/com/kavenegar/sdk/models/StatusLocalMessageIdResult.java
@@ -1,24 +1,77 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.kavenegar.sdk.models;
 
 import com.google.gson.JsonObject;
+import java.util.Objects;
 
 /**
- * @author mohsen
+ * Represents the status result of a message identified by a local ID.
+ * Extends the generic StatusResult with an additional localId field.
+ *
+ * The code was originally written by Mohsen and later developed further by Reza.
  */
 public class StatusLocalMessageIdResult extends StatusResult {
 
-    long localId;
+    private final long localId;
 
     public StatusLocalMessageIdResult(JsonObject json) {
         super(json);
-        this.localId = json.get("localid").getAsLong();
+        this.localId = json.has("localid") && !json.get("localid").isJsonNull()
+                ? json.get("localid").getAsLong() : -1L;
     }
 
     public long getLocalId() {
         return localId;
+    }
+
+    /**
+     * Validates whether the local ID is a positive value.
+     */
+    public boolean isValidLocalId() {
+        return localId > 0;
+    }
+
+    /**
+     * Provides a human-readable summary of the result.
+     */
+    public String getSummary() {
+        return String.format("Local ID: %d | Status: %s (%d)",
+                localId, getStatusText(), getStatus());
+    }
+
+    /**
+     * Compares the local ID with another result.
+     */
+    public boolean hasSameLocalId(StatusLocalMessageIdResult other) {
+        return other != null && this.localId == other.localId;
+    }
+
+    /**
+     * Checks if the status indicates failure.
+     */
+    public boolean isFailedStatus() {
+        return getStatus() != 1;
+    }
+
+    @Override
+    public String toString() {
+        return "StatusLocalMessageIdResult{" +
+                "localId=" + localId +
+                ", status=" + getStatus() +
+                ", statusText='" + getStatusText() + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof StatusLocalMessageIdResult)) return false;
+        if (!super.equals(o)) return false;
+        StatusLocalMessageIdResult that = (StatusLocalMessageIdResult) o;
+        return localId == that.localId;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), localId);
     }
 }
